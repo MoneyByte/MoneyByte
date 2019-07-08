8d2acbd6377a649b23e758238e663d6cb86c7e54
CircuitBreaker88 <sarris88p@gmail.com>
2019-07-07 20:10:20 -0400

    change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -91,17 +91,30 @@ public:
 
 		hashGenesisBlock = genesis.GetHash();
 		consensus.hashGenesisBlock = uint256S("0x");
-		if (genesis.nNonce != 0) {
-
-}
-
-if (genesis.nNonce == 0) {
-for(genesis.nNonce == 0; genesis.GetHash() > bnProofOfWorkLimit; genesis.nNonce++){ }
-printf("  nTime: %u\n", genesis.nTime);
-printf("  nNonce: %u\n", genesis.nNonce);
-printf("  Hash: %s\n", genesis.GetHash().ToString().c_str());
-printf("  hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
-}
+		if(genesis.GetHash() != uint256("0x"))
+		{
+		      printf("MSearching for genesis block...\n");
+		      uint256 hashTarget;
+		      hashTarget.SetCompact(genesis.nBits);
+		      while(uint256(genesis.GetHash()) > uint256(hashTarget))
+		      {
+		          ++genesis.nNonce;
+		          if (genesis.nNonce == 0)
+		          {
+		              printf("Mainnet NONCE WRAPPED, incrementing time");
+		              std::cout << std::string("Mainnet NONCE WRAPPED, incrementing time:\n");
+		              ++genesis.nTime;
+		          }
+		          if (genesis.nNonce % 10000 == 0)
+		          {
+		              printf("Mainnet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+		          }
+		      }
+		      printf("Mainnet block.nTime = %u \n", genesis.nTime);
+		      printf("Mainnet block.nNonce = %u \n", genesis.nNonce);
+		      printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+		      printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+		}
 		assert(genesis.hashMerkleRoot == uint256("0x"));
 		assert(hashGenesisBlock == uint256("0x"));
 
