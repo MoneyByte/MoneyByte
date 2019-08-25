267bda58f7941f940db704a51fa1e4246a3f0630
CircuitBreaker88 <sarris88p@gmail.com>
2019-08-25 15:13:42 -0400

    New Genesis

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -85,9 +85,37 @@ public:
 		genesis.nVersion = 1;
 		genesis.nTime = 1513634048; //
 		genesis.nBits = 520159231;
-		genesis.nNonce = 47950;
+		genesis.nNonce = 0;
 
+ if (true && genesis.GetHash() != hashGenesisBlock)
+                {
+                    printf("Searching for genesis block...\n");
+                    uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+                    uint256 thash;
 
+                    while (true)
+                    {
+                        thash = genesis.GetHash();
+                        if (thash <= hashTarget)
+                            break;
+                        if ((genesis.nNonce & 0xFFF) == 0)
+                        {
+                            printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                        }
+                        ++genesis.nNonce;
+                        if (genesis.nNonce == 0)
+                        {
+                            printf("NONCE WRAPPED, incrementing time\n");
+                            ++genesis.nTime;
+                        }
+                    }
+                    printf("genesis.nTime = %u \n", genesis.nTime);
+                    printf("genesis.nNonce = %u \n", genesis.nNonce);
+                    printf("genesis.nVersion = %u \n", genesis.nVersion);
+                    printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+                    printf("genesis.hashMerkleRoot = %s \n", genesis.hashMerkleRoot.ToString().c_str());
+
+                }
 
 		hashGenesisBlock = genesis.GetHash();
 
