3b84ea10f97577fe87e9126eda5b8534309528b3
CircuitBreaker88 <sarris88p@gmail.com>
2019-09-09 16:33:56 -0400

    Test for BlockCheck

--- src/main.cpp
+++ src/main.cpp
@@ -2865,9 +2865,9 @@ bool CBlock::CheckBlock(bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig) c
 
     if (IsProofOfStake())
     {
-        // Coinbase output should be empty if proof-of-stake block
-        if (vtx[0].vout.size() != 1 || !vtx[0].vout[0].IsEmpty())
-            return DoS(100, error("CheckBlock() : coinbase output not empty for proof-of-stake block"));
+      // Coinbase output should be empty if proof-of-stake block. DevFee creates 2nd coinbase tx
+      if (((vtx[0].vout.size() != 1) && (vtx[0].vout.size() != 2)) || !vtx[0].vout[0].IsEmpty())
+          return DoS(100, error("CheckBlock() (devFee) : coinbase output not empty for proof-of-stake block"));
 
         // Second transaction must be coinstake, the rest must not be
         if (vtx.empty() || !vtx[1].IsCoinStake())
